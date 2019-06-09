77bfa1e2b010f4c36b4c839358adf3fcfbae1b2b
jfkonecn <konecnyj@oregonstate.edu>
2019-06-09 13:54:06 -0400

    Db context added

---
+++ EngineeringMath/Model/EngineeringMathContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineeringMath.Model
+{
+    class EngineeringMathContext : DbContext
+    {
+        public DbSet<Equation> Equations { get; set; }
+        public DbSet<Function> Functions { get; set; }
+        public DbSet<FunctionCategory> FunctionCategories { get; set; }
+        public DbSet<ImportedClass> ImportedClasses { get; set; }
+        public DbSet<ImportedEquation> ImportedEquations { get; set; }
+        public DbSet<ImportedMethod> ImportedMethods { get; set; }
+        public DbSet<ImportedNamespace> ImportedNamespaces { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Parameter> Parameters { get; set; }
+        public DbSet<ParameterType> ParameterTypes { get; set; }
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<UnitCategory> UnitCategories { get; set; }
+        public DbSet<UnitSystem> UnitSystems { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=engineeringMath.db");
+        }
+    }
+}
