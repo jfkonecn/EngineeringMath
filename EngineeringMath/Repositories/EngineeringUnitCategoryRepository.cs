d8bf74d0d8da394b28b9cf277db9332e17ea4464
jfkonecn <konecnyj@oregonstate.edu>
2019-07-14 16:16:07 -0400

    Working on composite units

--- EngineeringMath/Repositories/EngineeringUnitCategoryRepository.cs
+++ EngineeringMath/Repositories/EngineeringUnitCategoryRepository.cs
@@ -2,7 +2,9 @@
 using EngineeringMath.Factories;
 using EngineeringMath.Loggers;
 using EngineeringMath.Model;
+using EngineeringMath.Resources;
 using EngineeringMath.Results;
+using StringMath;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,11 +18,13 @@ namespace EngineeringMath.Repositories
             IReadonlyRepository<UnitCategory> unitCategoryRepository,
             IReadonlyRepository<Unit> unitRepository,
             IResultFactory resultFactory,
+            IStringEquationFactory stringEquationFactory,
             ILogger logger)
         {
             UnitCategoryRepository = unitCategoryRepository;
             UnitRepository = unitRepository;
             ResultFactory = resultFactory;
+            StringEquationFactory = stringEquationFactory;
             Logger = logger;
         }
 
@@ -38,23 +42,57 @@ namespace EngineeringMath.Repositories
         
         public IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> GetAll()
         {
-            //var allCache = UnitCategoryCache.ToList();
-            throw new NotImplementedException();
+            List<EngineeringUnitCategory> allCache = UnitCategoryCache.ToList();
+            var existingNames = new HashSet<string>();
+            allCache.ForEach(x => existingNames.Add(x.Name));
+            bool whereCondition(UnitCategory x) => !existingNames.Contains(x.Name);
+            return GetFromRepositoryWhere(whereCondition);
+
         }
+
+
 
         public IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> GetAllWhere(Func<EngineeringUnitCategory, bool> whereCondition)
         {
-            //Logger.Debug("GetUnitCategory", $"Looking for {unitCategoryName}");
-            //var unitCategory = UnitCategoryCache.Where(x => x.Name == unitCategoryName).SingleOrDefault();
-            //if (unitCategory != null)
-            //{
-            //    return ResultFactory.BuilderResult(RepositoryStatusCode.success, RepositoryAction.Get, unitCategory);
-            //}
-            //var result = CreateUnitCategory(unitCategoryName);
-            //UnitCategoryCache.Add(result.ResultObject);
-            //return result;
-            throw new NotImplementedException();
+            IEnumerable<EngineeringUnitCategory> unitCategory = UnitCategoryCache.Where(whereCondition);
+            if (unitCategory.Count() > 0)
+            {
+                return ResultFactory.BuilderResult(RepositoryStatusCode.success, RepositoryAction.Get, unitCategory);
+            }
+            return ResultFactory.BuilderResult<IEnumerable<EngineeringUnitCategory>>(
+                RepositoryStatusCode.objectNotFound, RepositoryAction.Get, null);
+        }
 
+        private IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> GetFromRepositoryWhere(Func<UnitCategory, bool> whereCondition)
+        {
+            IResult<RepositoryStatusCode, IEnumerable<UnitCategory>> queryResult =
+                UnitCategoryRepository
+                .GetAllWhere(whereCondition);
+
+            if (queryResult.StatusCode != RepositoryStatusCode.success)
+            {
+                return ResultFactory
+                    .BuilderResult<IEnumerable<EngineeringUnitCategory>>(queryResult.StatusCode, RepositoryAction.Get, null);
+            }
+
+            IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> buildResult =
+                BuildEngineeringUnitCategory(queryResult.ResultObject);
+
+            if (buildResult.StatusCode != RepositoryStatusCode.success)
+            {
+                return ResultFactory
+                    .BuilderResult<IEnumerable<EngineeringUnitCategory>>(buildResult.StatusCode, RepositoryAction.Get, null);
+            }
+
+            foreach (EngineeringUnitCategory item in buildResult.ResultObject)
+            {
+                UnitCategoryCache.Add(item);
+            }
+            return ResultFactory
+                    .BuilderResult<IEnumerable<EngineeringUnitCategory>>(
+                        RepositoryStatusCode.success,
+                        RepositoryAction.Get,
+                        UnitCategoryCache);
         }
 
         /// <summary>
@@ -64,22 +102,111 @@ namespace EngineeringMath.Repositories
         /// <returns></returns>
         public IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> GetById(IEnumerable<object> keys)
         {
-            throw new NotImplementedException();
+            if (!(keys is IEnumerable<string> realKeys))
+            {
+                return ResultFactory
+                    .BuilderResult<IEnumerable<EngineeringUnitCategory>>(
+                    RepositoryStatusCode.objectNotFound,
+                    RepositoryAction.Get,
+                    null);
+            }
+            IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> result = 
+                GetFromRepositoryWhere(x => realKeys.Contains(x.Name));
+            if(result.StatusCode != RepositoryStatusCode.success || 
+                result.ResultObject.Count() != result.ResultObject.Count())
+            {
+                var statusCode = result.StatusCode != RepositoryStatusCode.success ? result.StatusCode : RepositoryStatusCode.objectNotFound;
+                return ResultFactory.BuilderResult<IEnumerable<EngineeringUnitCategory>>(statusCode, RepositoryAction.Get, null);
+            }
+            return ResultFactory.BuilderResult(RepositoryStatusCode.success, RepositoryAction.Get, result.ResultObject);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="keys">unitCategoryName</param>
+        /// <returns></returns>
         public IResult<RepositoryStatusCode, EngineeringUnitCategory> GetById(object key)
         {
-            throw new NotImplementedException();
+            IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> result = GetById(new List<object>(){ key });
+            EngineeringUnitCategory resultObject = result.ResultObject == null ? result.ResultObject.FirstOrDefault() : null;
+            return ResultFactory.BuilderResult(result.StatusCode, RepositoryAction.Get, resultObject);
         }
 
-        private IResult<RepositoryStatusCode, EngineeringUnitCategory> CreateUnitCategory(string unitCategoryName)
+        private IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> BuildEngineeringUnitCategory(IEnumerable<UnitCategory> blueprints)
         {
-            throw new NotImplementedException();
+            Stack<EngineeringUnitCategory> unitCategories = new Stack<EngineeringUnitCategory>();
+            foreach (UnitCategory blueprint in blueprints)
+            {
+                IResult<RepositoryStatusCode, IEnumerable<EngineeringUnit>> compositeResult = CreateCompositeUnits(blueprint);
+                if (compositeResult.StatusCode != RepositoryStatusCode.success)
+                {
+                    return ResultFactory.BuilderResult<IEnumerable<EngineeringUnitCategory>>(
+                            compositeResult.StatusCode, RepositoryAction.Get, null);
+                }
+
+
+            }
+            return ResultFactory.BuilderResult<IEnumerable<EngineeringUnitCategory>>(
+                RepositoryStatusCode.success, RepositoryAction.Get, unitCategories);
         }
+
+        private IResult<RepositoryStatusCode, IEnumerable<EngineeringUnit>> CreateCompositeUnits(UnitCategory blueprint)
+        {
+            Stack<EngineeringUnit> compositeUnits = new Stack<EngineeringUnit>();
+            if (!string.IsNullOrEmpty(blueprint.CompositeEquation))
+            {
+                IStringEquation stringEquation = StringEquationFactory.CreateStringEquation(blueprint.CompositeEquation);
+                IResult<RepositoryStatusCode, IEnumerable<EngineeringUnitCategory>> compositeResult = GetById(stringEquation.EquationArguments);
+                if (compositeResult.StatusCode != RepositoryStatusCode.success)
+                {
+                    return ResultFactory
+                        .BuilderResult<IEnumerable<EngineeringUnit>>(compositeResult.StatusCode, RepositoryAction.Get, null);
+                }
+                Dictionary<string, IEnumerable<EngineeringUnit>> dic = compositeResult
+                    .ResultObject
+                    .ToDictionary(cat => cat.Name, cat => cat.Units);
+
+                var unitCategories = new Dictionary<string, Dictionary<string, EngineeringUnit>>();
+
+                foreach (KeyValuePair<string, IEnumerable<EngineeringUnit>> item in dic)
+                {
+                    var units = new Dictionary<string, EngineeringUnit>();
+                    unitCategories.Add(item.Key, units);
+                    foreach (var unit in item.Value)
+                    {
+                        foreach (var system in unit.UnitSystems)
+                        {
+                            bool allowSystemForComposite = system != nameof(LibraryResources.ImperialFullName) &&
+                                system != nameof(LibraryResources.MetricFullName);
+
+                            if (allowSystemForComposite && units.ContainsKey(system))
+                            {
+                                Logger.Error("CreateCompositeUnits", $"More than one unit using {system} as a system!");
+                                return ResultFactory.BuilderResult<IEnumerable<EngineeringUnit>>(
+                                    RepositoryStatusCode.internalError, RepositoryAction.Get, null);
+
+                            }
+                            else if (allowSystemForComposite && !units.ContainsKey(system))
+                            {
+                                units.Add(system, unit);
+                            }
+                        }
+                    }
+                }
+
+            }
+            return ResultFactory.BuilderResult<IEnumerable<EngineeringUnit>>(
+                RepositoryStatusCode.success, RepositoryAction.Get, compositeUnits);
+
+        }
+
+
 
         private IReadonlyRepository<UnitCategory> UnitCategoryRepository { get; }
         private IReadonlyRepository<Unit> UnitRepository { get; }
         public IResultFactory ResultFactory { get; }
+        public IStringEquationFactory StringEquationFactory { get; }
         private ILogger Logger { get; }
         private HashSet<EngineeringUnitCategory> UnitCategoryCache { get; } = new HashSet<EngineeringUnitCategory>();
     }
