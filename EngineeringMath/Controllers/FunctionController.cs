64d6d2ace8cb57087bb0375bef11b97d396b80b5
jfkonecn <konecnyj@oregonstate.edu>
2019-09-21 19:42:10 -0400

    Finished evaluate function

--- EngineeringMath/Controllers/FunctionController.cs
+++ EngineeringMath/Controllers/FunctionController.cs
@@ -2,6 +2,7 @@
 using EngineeringMath.Model;
 using EngineeringMath.Repositories;
 using EngineeringMath.Results;
+using EngineeringMath.Validators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,10 @@ namespace EngineeringMath.Controllers
 {
     public class FunctionController
     {
-        public FunctionController(IReadonlyRepository<Function> functionRepository)
+        public FunctionController(IReadonlyRepository<Function> functionRepository, IValidator<Parameter> parameterValidator)
         {
             FunctionRepository = functionRepository;
+            ParameterValidator = parameterValidator;
         }
 
         /// <summary>
@@ -36,10 +38,26 @@ namespace EngineeringMath.Controllers
 
         public void Evaluate()
         {
-            Equation.Formula.Evaluate();
+            Dictionary<string, Parameter> curParams = Function
+                .Parameters
+                .Where(x => Equation.Formula.EquationArguments
+                .Contains(x.KeyName))
+                .ToDictionary(x => x.KeyName, x => x);
+            foreach (Parameter parameter in curParams.Values)
+            {
+                ParameterValidator.Validate(parameter);
+            }
+            var paraArr = new double[curParams.Values.Count()];
+            for (int i = 0; i < paraArr.Length; i++)
+            {
+                string parameterKey = Equation.Formula.EquationArguments[i];
+                paraArr[i] = curParams[parameterKey].Value;
+            }
+            Equation.Formula.Evaluate(paraArr);
         }
 
         private IReadonlyRepository<Function> FunctionRepository { get; }
+        public IValidator<Parameter> ParameterValidator { get; }
         private Function Function { get; set; }
         private Equation Equation { get; set; }
     }
